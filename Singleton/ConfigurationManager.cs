4681367d6077ee5ae2044744fb672da36a3506e6
Nassar <mohammednassar772003@gmail.com>
2025-06-09 19:17:14 +0300

    add singleton pattern  explain a ship with 2 captains sinks

---
+++ Singleton/ConfigurationManager.cs
@@ -0,0 +1,35 @@
+﻿public class ConfigurationManager
+{
+    private static ConfigurationManager _instance;
+    private static readonly object _lock = new object();
+
+    public string DatabaseConnectionString { get; private set; }
+    public string ApplicationName { get; private set; }
+
+    // المُنشئ خاص لمنع إنشاء الكائن من الخارج
+    private ConfigurationManager()
+    {
+        // محاكاة تحميل الإعدادات
+        Console.WriteLine("Loading configuration...");
+        DatabaseConnectionString = "Server=.;Database=MyApp;Trusted_Connection=True;";
+        ApplicationName = "My Cool App";
+    }
+
+    // الوصول إلى الكائن الوحيد
+    public static ConfigurationManager GetInstance()
+    {
+        if (_instance == null)
+        {
+            lock (_lock)
+            {
+                if (_instance == null)
+                {
+                    _instance = new ConfigurationManager();
+                }
+            }
+        }
+
+        return _instance;
+    }
+}
+
---
+++ Singleton/Program.cs
@@ -0,0 +1,16 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        var config1 = ConfigurationManager.GetInstance();
+        var config2 = ConfigurationManager.GetInstance();
+
+        Console.WriteLine(config1.ApplicationName);
+        Console.WriteLine(config1.DatabaseConnectionString);
+
+        // تحقق أن الكائنين هما نفس المرجع
+        Console.WriteLine(object.ReferenceEquals(config1, config2)); // True
+    }
+}
+
